a188ff4e5a16737c7e269691419816c13e2a87c6
Jessica <jess.newman19@gmail.com>
2022-06-28 10:57:26 -0400

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get '/hello', to: 'application#hello_world'
 
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
